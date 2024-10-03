d797a232fc4d74e684db3cb0edaadf6fbd5c54f6
Rajasekhar-coder <rajrajasekhar1@outlook.com>
2024-10-03 16:03:14 +0530

    changes

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ package java_Projects;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to my github!");
     }
 }
